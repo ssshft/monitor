5365262b346c87f7b6c51507d2a826bd0b75c940
sss <songsensen1@163.com>
2026-04-16 14:45:05 +0800

    update

--- script/larkrobot/run.py
+++
@@ -1,9 +0,0 @@
-from api.larkcustom import LarkCustomBot
-import asyncio
-
-webhook_url = 'https://open.larksuite.com/open-apis/bot/v2/hook/ae37233c-b5b1-4b9a-a9f4-820436367989'
-webhook_secret = 'PaQY1h1MURrO24YZaYNlPb'
-
-if __name__ == '__main__':
-    lark = LarkCustomBot(webhook_url, webhook_secret)
-    asyncio.run(lark.send_text("this is a test!", True))
