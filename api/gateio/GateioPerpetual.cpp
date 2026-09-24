7e34d752a047ca1e3908a5991e4a6dca902e6b36
lawson <lawson@192.168.1.10>
2026-09-24 22:08:17 +0800

    update

--- api/gateio/GateioPerpetual.cpp
+++ api/gateio/GateioPerpetual.cpp
@@ -109,7 +109,7 @@ bool GateioPerpetual::QueryPosition(vector<gateio::FuturePosition>& vFuturePosit
     std::string body;
 
     std::string time_str = std::to_string(crypto::getCurrentTimeSeconds());    
-    std::string sign = crypto::getGateioSignatureRest("GET", positionUrl, time_str, "status=open", "", accountInfo.secretKey);
+    std::string sign = crypto::getGateioSignatureRest("GET", positionUrl, time_str, "", "", accountInfo.secretKey);
     std::vector<std::pair<std::string, std::string>> headers = {{"KEY", accountInfo.apiKey}, {"Timestamp", time_str}, {"SIGN", sign}};
 
     try {
@@ -246,7 +246,7 @@ bool GateioPerpetual::QueryOpenOrder(vector<gateio::FutureOrder>& vFutureOrder, 
     std::string body;
 
     std::string time_str = std::to_string(crypto::getCurrentTimeSeconds());    
-    std::string sign = crypto::getGateioSignatureRest("GET", orderUrl, time_str, "", "", accountInfo.secretKey);
+    std::string sign = crypto::getGateioSignatureRest("GET", orderUrl, time_str, "status=open", "", accountInfo.secretKey);
     std::vector<std::pair<std::string, std::string>> headers = {{"KEY", accountInfo.apiKey}, {"Timestamp", time_str}, {"SIGN", sign}};
 
     try {
